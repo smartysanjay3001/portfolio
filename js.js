a6eb5e2f7e7192ccbbc40aafcea1adc61504ba3b
smartysanjay3001 <sanjaykmr3001@gmail.com>
2023-04-11 07:45:48 +0530

    portfolio

---
+++ js.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".autoinput",{
+  strings:[" Hi..i am SanjayKumar","Front-End Web Developer" ],
+
+  typeSpeed:100,
+  backSpeed:50,
+  loop:true
+})                                                                                                                                                            
